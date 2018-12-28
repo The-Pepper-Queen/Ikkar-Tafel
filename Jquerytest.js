72aea04e3d13a8dfb62976eee69f53f5ae0ebc50
The-Pepper-Queen <32080395+The-Pepper-Queen@users.noreply.github.com>
2018-12-27 23:19:45 -0800

    Add files via upload

---
+++ Jquerytest.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    $("#qu").click(function(){
+    $("h1").toggle();
+    });
+    $("#submit").click(displayBracha).click(displayFood).click(appendBracha).click(appendFood)
+});
+
+function displayFood() {
+    var x = document.getElementById("food1");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length ;i++) {
+     text += x.elements[i].value + ",";
+    }
+    document.getElementById("foodlist").innerHTML += text;
+    document.getElementById("foodhiddenlist").innerHTML = text;
+}
+
+function displayBracha() {
+    var x = document.getElementById("bracha1");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length ;i++) {
+     text += x.elements[i].value + ",";
+    }
+    document.getElementById("brachalist").innerHTML += text;
+    document.getElementById("brachahiddenlist").innerHTML = text;
+}
+
+var bracha = [];
+var food = [];
+
+function appendBracha() {
+var y = document.getElementById("brachahiddenlist").innerHTML;
+bracha.push(y);
+alert(bracha);
+}
+function appendFood() {
+    var y = document.getElementById("foodhiddenlist").innerHTML;
+    food.push(y);
+   // alert(food);
+}
+
+function brachaEval() {
+
+}
